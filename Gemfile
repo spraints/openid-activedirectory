42f6bcd32fbb624f67ee561565f065154afc42a4
Matt Burke <maburke@sep.com>
2010-07-19 12:57:07 -0400

    Implemented the main logic of authentication.

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 gem 'rack', '1.2.0'
 gem 'sinatra'
+gem 'haml'
--- app.rb
+++ app.rb
@@ -2,8 +2,30 @@ require 'rubygems'
 require 'bundler'
 Bundler.setup
 require 'sinatra'
+require 'haml'
+
+helpers do
+  def current_user
+    System::Web::HttpContext.current.user.identity.name
+  rescue
+    nil
+  end
+end
 
 get '/' do
-  content_type :text
-  'hello!'
+  @user = current_user
+  if @user.nil? || @user.empty?
+    haml :not_logged_in
+  else
+    return redirect "/#{@user}"
+  end
+end
+
+get '/:username' do
+  @requested_user = params[:username]
+  if @requested_user == current_user
+    haml :me
+  else
+    haml :not_me
+  end
 end
